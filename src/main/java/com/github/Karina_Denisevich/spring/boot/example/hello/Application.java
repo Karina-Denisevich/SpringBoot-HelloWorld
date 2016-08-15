7ed806b617b941c7823e98685c48df86b30203e9
Karina Denisevich <karina_41997@mail.ru>
2016-08-16 00:41:34 +0300

    master: app and controller classes added

--- src/main/java/com/github/Karina_Denisevich/spring/boot/example/hello/Application.java
+++ src/main/java/com/github/Karina_Denisevich/spring/boot/example/hello/Application.java
@@ -1,4 +1,14 @@
 package com.github.Karina_Denisevich.spring.boot.example.hello;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    }
 }
--- src/main/java/com/github/Karina_Denisevich/spring/boot/example/hello/HelloController.java
+++ src/main/java/com/github/Karina_Denisevich/spring/boot/example/hello/HelloController.java
@@ -1,4 +1,14 @@
 package com.github.Karina_Denisevich.spring.boot.example.hello;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
 public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+
+        return "Hello World!";
+    }
 }
